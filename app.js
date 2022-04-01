d2d313f127672ea9a5e485c92f00f1d697f3d78c
Shimon Cohen <shimoco7@cs.colman.ac.il>
2022-04-01 14:33:56 +0300

    MongoDB initial infrastructure

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
-const appRouter = require('./routes/router');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT
 
@@ -9,4 +9,14 @@ app.listen(port,()=>{
     console.log('Server is running on port ' + port);
 });
 
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true});
+const db = mongoose.connection;
+db.on('error',error=>{console.log(error)});
+db.on('open',()=>{console.log('Connected to MongoDB')});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true,limit:'1m'}));
+app.use(bodyParser.json());
+
+const appRouter = require('./routes/router');
 app.use('/',appRouter);
