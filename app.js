2c24b99312669f8ffd571c7c8b95c564c6d03574
Shimon Cohen <shimoco7@cs.colman.ac.il>
2022-04-01 12:36:12 +0300

    initial app architecture

---
+++ app.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+const appRouter = require('./routes/router');
+
+const port = process.env.PORT
+
+app.listen(port,()=>{
+    console.log('Server is running on port ' + port);
+});
+
+app.use('/',appRouter);
