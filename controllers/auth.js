b64f758ee88bf7fcac68971936f0588ba28cf79d
adargvili <adar555@gmail.com>
2022-04-02 17:00:12 +0300

    main

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,9 @@ const User = require('../models/user_model')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-const sendError = (res,code,msg)=>{
+let refreshTokens = []
+
+const sendError = (res, code, msg) => {
     return res.status(code).send({
         'status': 'fail',
         'error': msg
@@ -13,25 +15,25 @@ const register = async (req, res) => {
     const email = req.body.email
     const password = req.body.password
 
-    try{
-        const exists = await User.findOne({'email' : email})
-        if (exists != null){
+    try {
+        const exists = await User.findOne({ 'email': email })
+        if (exists != null) {
             return res.status(400).send({
                 'status': 'fail',
                 'error': 'user exists'
             })
         }
         const salt = await bcrypt.genSalt(10)
-        const hashPwd = await bcrypt.hash(password,salt)
+        const hashPwd = await bcrypt.hash(password, salt)
 
         const user = User({
-            'email' : email,
+            'email': email,
             'password': hashPwd
         })
         newUser = await user.save();
         res.status(200).send(newUser)
 
-    }catch(err){
+    } catch (err) {
         res.status(400).send({
             'status': 'fail',
             'error': err.message
@@ -43,37 +45,60 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     const email = req.body.email
     const password = req.body.password
-    if (email == null || password == null) return sendError(res,400,'wrong email or password')
-    
-    try{
-        const user = await User.findOne({'email' : email })
-        if (user == null) return sendError(res,400,'wrong email or password')
+    if (email == null || password == null) return sendError(res, 400, 'wrong email or password')
+
+    try {
+        const user = await User.findOne({ 'email': email })
+        if (user == null) return sendError(res, 400, 'wrong email or password')
 
         const match = await bcrypt.compare(password, user.password)
-        if (!match) return sendError(res,400,'wrong email or password')
+        if (!match) return sendError(res, 400, 'wrong email or password')
 
-        const accessToken = await jwt.sign(
-            {'id':user._id},
-            process.env.ACCESS_TOKEN_SECRET,
-            {expiresIn: process.env.JWT_TOKEN_EXPIRATION}
-            )
-        res.status(200).send({'accessToken' : accessToken})
+        const accessToken = generateAccessToken(user)
+        const refreshToken = await jwt.sign(
+            { 'id': user._id },
+            process.env.REFRESH_TOKEN_SECRET
+        )
+        refreshTokens.push(refreshToken)
+        res.status(200).send({ 'accessToken': accessToken, 'refreshToken': refreshToken })
 
-    }catch(err){
-        return sendError(res,400,err.message)
+    } catch (err) {
+        return sendError(res, 400, err.message)
     }
 
 }
 
+const token = async (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return sendError(res, 401, 'no refresh token')
+    if (!refreshTokens.includes(refreshToken)) return sendError(res, 403, 'no refresh token')
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return sendError(res, 403, fail)
+        const accessToken = generateAccessToken({ 'id': user._id })
+        res.status(200).send({ 'accessToken': accessToken})
+
+    }
+    )
+}
+
 const logout = async (req, res) => {
-    res.status(400).send({
-        'status': 'fail',
-        'error': 'not implemented'
-    })
+    if (refreshTokens.includes(req.body.token)){
+        refreshTokens = refreshTokens.filter(token=>token!==req.body.token)
+    }
+    res.status(204)
+}
+
+function generateAccessToken(user) {
+    return jwt.sign(
+        { 'id': user._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.JWT_TOKEN_EXPIRATION }
+    )
 }
 
 module.exports = {
     login,
     register,
-    logout
+    logout,
+    token
 }
