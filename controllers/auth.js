15dfe09d691a9b5f73c7da478a5b1686234612d8
adargvili <adar555@gmail.com>
2022-04-03 12:26:21 +0300

    temp logout and refresh updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,6 @@ const User = require('../models/user_model')
 const bcryptjs = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-let refreshTokens = []
-
 const sendError = (res, code, msg) => {
     return res.status(code).send({
         'status': 'fail',
@@ -24,7 +22,7 @@ const register = async (req, res) => {
             })
         }
         const salt = await bcryptjs.genSalt(10)
-        const hashPwd = await bcryptjs.hash(password,salt)
+        const hashPwd = await bcryptjs.hash(password, salt)
 
         const user = User({
             'email': email,
@@ -55,11 +53,10 @@ const login = async (req, res) => {
         if (!match) return sendError(res, 400, 'wrong email or password')
 
         const accessToken = generateAccessToken(user)
-        const refreshToken = await jwt.sign(
-            { 'id': user._id },
-            process.env.REFRESH_TOKEN_SECRET
-        )
-        refreshTokens.push(refreshToken)
+        const refreshToken = generateRefreshToken(user)
+        if (user.tokens == null) user.tokens = [refreshToken]
+        else user.tokens.push(refreshToken)
+        await user.save()
         res.status(200).send({ 'accessToken': accessToken, 'refreshToken': refreshToken })
 
     } catch (err) {
@@ -69,23 +66,57 @@ const login = async (req, res) => {
 }
 
 const refreshToken = async (req, res) => {
-    const refreshToken = req.body.token
-    if (refreshToken == null) return sendError(res, 401, 'no refresh token')
-    if (!refreshTokens.includes(refreshToken)) return sendError(res, 403, 'no refresh token')
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) return sendError(res, 403, fail)
-        const accessToken = generateAccessToken({ 'id': user._id })
-        res.status(200).send({ 'accessToken': accessToken})
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return sendError(res, 401, 'no refresh token')
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, async (err, userInfo) => {
 
-    }
-    )
+        if (err) return res.status(403).send(err.message)
+        const userId = userInfo.id
+        try {
+            user = await User.findById(userId)
+            if (user == null) return res.status(403).send('invalid request')
+            if (!user.tokens.includes(token)) {
+                user.tokens = []
+                await user.save()
+                return res.status(403).send('invalid request')
+            }
+
+            const accessToken = generateAccessToken(user)
+            const refreshToken = generateRefreshToken(user)
+            user.tokens[user.tokens.indexOf(token)] = refreshToken
+            await user.save()
+            res.status(200).send({ 'accessToken': accessToken, 'refreshToken': refreshToken });
+        } catch (err) {
+            res.status(403).send(err.message)
+        }
+    })
 }
 
 const logout = async (req, res) => {
-    if (refreshTokens.includes(req.body.token)){
-        refreshTokens = refreshTokens.filter(token=>token!==req.body.token)
-    }
-    res.status(204)
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus('401')
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, async (err, userInfo) => {
+
+        if (err) return res.status(403).send(err.message)
+        const userId = userInfo.id
+        try {
+            user = await User.findById(userId)
+            if (user == null) return res.status(403).send('invalid request')
+            if (!user.tokens.includes(token)) {
+                user.tokens = []
+                await user.save()
+                return res.status(403).send('invalid request')
+            }
+            user.tokens.splice(user.tokens.indexOf(token), 1)
+            await user.save()
+            res.status(200).send();
+        } catch (err) {
+            res.status(403).send(err.message)
+        }
+    })
+
 }
 
 function generateAccessToken(user) {
@@ -93,6 +124,13 @@ function generateAccessToken(user) {
         { 'id': user._id },
         process.env.ACCESS_TOKEN_SECRET,
         { expiresIn: process.env.JWT_TOKEN_EXPIRATION }
+    )
+}
+
+function generateRefreshToken(user) {
+    return jwt.sign(
+        { 'id': user._id },
+        process.env.REFRESH_TOKEN_SECRET
     )
 }
 
