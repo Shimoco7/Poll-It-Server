81b2c0929217ae354bfa4fa0ff5861c5e1b70c64
adargvili <adar555@gmail.com>
2022-04-16 19:50:54 +0300

    minor auth change

--- routes/answer_routes.js
+++ routes/answer_routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Answer = require('../controllers/answer_controller');
 const authenticate = require("../common/auth_middleware");
 
-router.post('/create', Answer.create);
-router.get('/create', Answer.getCreate);
+router.post('/create', authenticate, Answer.create);
+router.get('/create', authenticate, Answer.getCreate);
 
 module.exports = router
