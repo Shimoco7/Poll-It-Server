b523f8375a76ed5383896ad103ddc75733c379d1
adar <adar555@gmail.com>
2022-04-22 19:06:36 +0300

    /

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 var multer = require('multer');
+const authenticate = require('../common/auth_middleware');
+const constants = require('../common/constants');
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'storage/images')
     },
     filename: function (req, file, cb) {
-      cb(null, Date.now() + '.jpg') //Appending .jpg
+      cb(null, Date.now() + '.jpg')
     }
   })
   
@@ -16,7 +18,7 @@ router.get('/',(req,res)=>{
     res.send("HELLO!");
 });
 
-router.post('/upload',upload.single('file'),(req,res)=>{
+router.post('/upload',authenticate([constants.USER,constants.CLIENT]),upload.single('file'),(req,res)=>{
     res.status(200).send({
         url: process.env.SERVER_URL +"/"+req.file.path.replace(/\\/g, "/")
     });
