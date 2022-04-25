c7d0a6f1432f97202b2828f096cae619cf26777b
adar <adar555@gmail.com>
2022-04-25 20:16:39 +0300

    added auth to public folder

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const answerRouter = require('./routes/answer_routes');
 const authenticate = require("./common/auth_middleware");
 const constants = require("./common/constants");
 
-app.use("/storage", express.static(path.resolve(__dirname + '/storage')));
+app.use("/storage",authenticate([constants.USER]), express.static(path.resolve(__dirname + '/storage')));
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
