df207561bd21aa7b58d34473db61186ebe0ca2c5
adar <adar555@gmail.com>
2022-05-06 16:27:29 +0300

    /

--- controllers/poll_controller.js
+++ controllers/poll_controller.js
@@ -18,8 +18,8 @@ const create = async (req, res) => {
     const permanentJob = req.body.permanentJob;
     const income = req.body.income;
     try {
-        newPoll = await Poll.create({ pollName: pollName, accountId: accountId, gender: gender, maritalStatus: maritalStatus, numberOfChildrens: numberOfChildrens, permanentJob: permanentJob , income: income, age: age, educationLevel: educationLevel });
-        return res.status(200).send({_id: newPoll._id});
+        newPoll = await Poll.create({ pollName: pollName, accountId: accountId, gender: gender, maritalStatus: maritalStatus, numberOfChildrens: numberOfChildrens, permanentJob: permanentJob, income: income, age: age, educationLevel: educationLevel });
+        return res.status(200).send({ _id: newPoll._id });
 
     } catch (err) {
         const erros = helpers.handleErrors(SCHEMA, err);
@@ -46,7 +46,7 @@ const getAllPolls = async (req, res) => {
 const getPollsByClientId = async (req, res) => {
     const accountId = req.params.accountId;
     try {
-        const polls = await Poll.find({accountId: accountId});
+        const polls = await Poll.find({ accountId: accountId });
         return res.status(200).send(polls);
 
     } catch (err) {
@@ -55,38 +55,37 @@ const getPollsByClientId = async (req, res) => {
     }
 }
 
-const getPollsByUserId = async (req, res) => { 
+const getPollsByUserId = async (req, res) => {
     const accountId = req.params.accountId;
     try {
         var matchedPolls = [];
         var detailsMap = {};
-        const account = await Account.findOne({_id: accountId, role: constants.USER});
+        const account = await Account.findOne({ _id: accountId, role: constants.USER });
         if (!account) return helpers.sendError(res, 400, 'account not found')
-        const details = await Detail.find({ accountId : account._id});
-        for (const detail of details){
-            detailsMap[constants.DETAIL_QUESTION_MAP[detail.question]]= detail.answer;
+        const details = await Detail.find({ accountId: account._id });
+        for (const detail of details) {
+            detailsMap[constants.DETAIL_QUESTION_MAP[detail.question]] = detail.answer;
         }
-        const polls = await Poll
-        .find({ 
-                age : {$in: [detailsMap[constants.AGE]]},
-                educationLevel : {$in: [detailsMap[constants.EDUCATION_LEVEL]]},
-                gender : {$in: [detailsMap[constants.GENDER]]},
-                maritalStatus : {$in: [detailsMap[constants.MARITAL_STATUS]]},
-                numberOfChildrens : {$in: [detailsMap[constants.NUMBER_OF_CHILDRENS]]},
-                permanentJob : {$in: [detailsMap[constants.PERMANENT_JOB]]},
-                income : {$in: [detailsMap[constants.INCOME]]},
+        const polls = await Poll.find({
+            age: { $in: [detailsMap[constants.AGE]] },
+            educationLevel: { $in: [detailsMap[constants.EDUCATION_LEVEL]] },
+            gender: { $in: [detailsMap[constants.GENDER]] },
+            maritalStatus: { $in: [detailsMap[constants.MARITAL_STATUS]] },
+            numberOfChildrens: { $in: [detailsMap[constants.NUMBER_OF_CHILDRENS]] },
+            permanentJob: { $in: [detailsMap[constants.PERMANENT_JOB]] },
+            income: { $in: [detailsMap[constants.INCOME]] },
         });
-        const answers = await Answer.find({ accountId : account._id});
-        for (const poll of polls){
+        const answers = await Answer.find({ accountId: account._id });
+        for (const poll of polls) {
             var filledPoll = false;
-            for(const answer of answers){
-                if (poll._id == answer.pollId){
+            for (const answer of answers) {
+                if (poll._id == answer.pollId) {
                     filledPoll = true;
                     break;
                 }
             }
-            if(!filledPoll){
-            matchedPolls.push(poll);
+            if (!filledPoll) {
+                matchedPolls.push(poll);
             }
         }
 
@@ -100,7 +99,7 @@ const getPollsByUserId = async (req, res) => {
 
 const update = async (req, res) => {
     try {
-        const updatedPoll = await Poll.findOneAndUpdate({ _id: req.body._id }, req.body, { returnOriginal: false, runValidators: true  });
+        const updatedPoll = await Poll.findOneAndUpdate({ _id: req.body._id }, req.body, { returnOriginal: false, runValidators: true });
         return res.status(200).send(updatedPoll);
 
     } catch (err) {
