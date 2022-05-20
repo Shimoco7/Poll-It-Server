edfded91edc76e83183c8b2243bb5fe5348fa8d3
adar <adar555@gmail.com>
2022-05-20 14:32:57 +0300

    /

--- routes/detail_question_routes.js
+++ routes/detail_question_routes.js
@@ -10,6 +10,47 @@ const constants = require('../common/constants');
 *   description: The DetailQuestion API
 */
 
+/**
+* @swagger
+* /detail_question/create:
+*  post:
+*     summary: Create a detail question
+*     description: "Roles: [Admin]"
+*     tags: [DetailQuestion API]
+*     requestBody:
+*        required: true
+*        content:
+*           application/json:
+*            schema:
+*                $ref: '#components/schemas/DetailQuestion'
+*     responses:
+*       200:
+*         description: DetailQuestion has been created
+*       400:
+*         description: Bad Request
+*       401:
+*         description: Unauthorized
+*       403:
+*         description:  Forbidden
+*       404:
+*         description:  Not Found
+*  get:
+*     summary: Create a detail question
+*     description: "Roles: [Admin]"
+*     tags: [DetailQuestion API]
+*     responses:
+*       200:
+*         description: DetailQuestion has been created
+*       400:
+*         description: Bad Request
+*       401:
+*         description: Unauthorized
+*       403:
+*         description:  Forbidden
+*       404:
+*         description:  Not Found
+*/
+
 router.post('/create', authenticate([constants.ADMIN]), DetailQuestion.create);
 
 router.get('/create', authenticate([constants.ADMIN]), DetailQuestion.getCreate);
